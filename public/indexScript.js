e3fcf8ce8ca38e7db54a76a78e29e568e05642da
Angello <agomezmonsalveangello@cesde.net>
2024-06-02 21:55:51 -0500

    sidebar con boton de volver funcional

--- public/indexScript.js
+++ public/indexScript.js
@@ -1,14 +1,25 @@
 let buttons = document.querySelectorAll('.entitie')
-
-buttons.forEach(button=>{
-    button.addEventListener('click',showSideBar)
+// add functionality to entitie buttons.
+buttons.forEach(button => {
+    button.addEventListener('click', showSideBar)
 })
 
-function showSideBar(){
-    console.log('me active')
+function showSideBar() {
+
     document.getElementById('main-container').classList.toggle('configure-main-container')
-   console.log( document.getElementById('main-container'))
+
     document.getElementById('side-bar').classList.toggle('hidden')
-    let uls=document.querySelectorAll('.button-list')
+    let uls = document.querySelectorAll('.button-list')
+    uls.forEach(ul => ul.classList.toggle('hidden'))
+}
+
+// add functionality to back-button in side-bar
+
+document.getElementById('back-button').addEventListener('click',hideSideBar)
+
+function hideSideBar(){
+    document.getElementById('main-container').classList.toggle('configure-main-container')
+    document.getElementById('side-bar').classList.toggle('hidden')
+    let uls = document.querySelectorAll('.button-list')
     uls.forEach(ul => ul.classList.toggle('hidden'))
 }
